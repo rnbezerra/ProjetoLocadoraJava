45a45b3f14df8ae1dc58492f4a061cd13bbc44ed
Ramonjija <ramonjija@gmail.com>
2013-05-28 23:35:34 -0300

    Criação das classes de leitura Ator e Diretor
    Essas classes são responsáveis por obter os dados do arquivo de texto.
    Para isso, utiliza como parametro um inteiro de 1-4 em Diretor e 1-6 em
    Artista, esse parametro define o campo ex: 1 - Codigo, 2 - Nome, 3 -
    Sexo etc. A descrição dos campos está no código

---
+++ src/importacao/AtorArtistaImportacao.java
@@ -0,0 +1,78 @@
+package importacao;
+
+import java.io.*;
+
+public class AtorArtistaImportacao {
+
+	/*O metodo dadosDiretor abre o arquivo diretor.txt e extrai os seguintes campos utilizando um valor de 
+	 * 1 - 6 como parametro:
+	 * 
+	 * 1 - Codigo do ator
+	 * 2 - Primeiro nome do ator
+	 * 3 - Nome completo do ator
+	 * 4 - Sexo ator
+	 * 5 - Data nascimento ator
+	 * 6 - Codigo dos filmes atuados
+	 * 
+	 * */
+	public static String dadosDiretor(int opcao){
+			
+			File dir = new File("C:\\importacao");
+			File arq = new File(dir, "atorArtista.txt");
+			
+			try{
+				
+				FileReader fileReader = new FileReader(arq);
+				
+				String[] linhaDoArquivo = new BufferedReader(fileReader).readLine().split(";");
+				switch (opcao) {
+				case 1:
+					String codigoAtor = linhaDoArquivo[0];
+					return codigoAtor;
+				
+				case 2:
+					String pNomeAtor = linhaDoArquivo[1];
+					return pNomeAtor;
+					
+				case 3:
+					String nomeCompAtor = linhaDoArquivo[2];
+					return nomeCompAtor;
+					
+				case 4:
+					String sexoAtor = linhaDoArquivo[3];
+					return sexoAtor;
+					
+				case 5:
+					String nascAtor = linhaDoArquivo[4];
+					return nascAtor;
+				
+				case 6:
+					String codFilmesAtor = linhaDoArquivo[5];
+					return codFilmesAtor;
+					
+				default:
+					System.out.println("Escolha um valor entre 1-6");
+					break;
+				}
+				
+				
+				}catch(IOException e){
+					
+					e.getStackTrace();
+					System.out.println("Erro em obter o código");
+					
+				}
+			return null;
+		} 
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(dadosDiretor(6));
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/importacao/DiretorImportacao.java
@@ -0,0 +1,65 @@
+package importacao;
+
+import java.io.*;
+
+
+public class DiretorImportacao {
+/*O metodo dadosDiretor abre o arquivo diretor.txt e extrai os seguintes campos utilizando um valor de 
+ * 1 - 4 como parametro:
+ * 
+ * 1 - Codigo do diretor
+ * 2 - Nome do diretor
+ * 3 - Sexo do diretor
+ * 4 - Codigo de filmes dirigidos
+ * 
+ * */
+public static String dadosDiretor(int opcao){
+		
+		File dir = new File("C:\\importacao");
+		File arq = new File(dir, "diretor.txt");
+		
+		try{
+			
+			FileReader fileReader = new FileReader(arq);
+			
+			String[] linhaDoArquivo = new BufferedReader(fileReader).readLine().split(";");
+			switch (opcao) {
+			case 1:
+				String codigoDir = linhaDoArquivo[0];
+				return codigoDir;
+			
+			case 2:
+				String NomeDir = linhaDoArquivo[1];
+				return NomeDir;
+				
+			case 3:
+				String sexoDir = linhaDoArquivo[2];
+				return sexoDir;
+				
+			case 4:
+				String filmesDir = linhaDoArquivo[3];
+				return filmesDir;
+								
+			default:
+				System.out.println("Escolha um valor entre 1-4");
+				break;
+			}
+			
+			}catch(IOException e){
+				
+				e.getStackTrace();
+				System.out.println("Erro em obter o código");
+				
+			}
+		return null;
+	} 
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(dadosDiretor(1));
+		// TODO Auto-generated method stub
+
+	}
+
+}
