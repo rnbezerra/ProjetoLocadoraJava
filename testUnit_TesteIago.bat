d3a19458cc473aaaf43c1fd11878b94ea47a8657
Rafael Nascimento Bezerra <rafael.bezerra@vtex.com.br>
2013-06-16 17:19:20 -0300

    Release #1.1.1
    reajustes menores nos arquivos da pasta

--- testUnit_TesteIago.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-	java -jar locadoraDVD.jar -comando alugarDVD -d 007 -c 002002 -t 14/06/2013 -v 0,00
-	java -jar locadoraDVD.jar -comando consultaCliente -c 002002
-	java -jar locadoraDVD.jar -comando selecionaDVD -c 007
-	java -jar locadoraDVD.jar -comando devolverDVD -d 007 -c 002002 -t 14/06/2013 -v 7,00
-PAUSE
